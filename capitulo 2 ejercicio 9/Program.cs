ef7ccb96a12de8d936f197afa10c0073062bb3f9
Hp <Hp@DESKTOP-NVV8KB0>
2022-11-13 21:10:46 -0500

    Agregar archivos de proyecto.

---
+++ capitulo 2 ejercicio 9/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ejercicio9
+    {
+    public class sueldo
+    {
+        public static void Main()
+        {
+            int SS, CH, VH;
+            Console.WriteLine("por favor digite las horas trabajadas");
+            CH = int.Parse(Console.ReadLine());
+            Console.WriteLine("por favor digite el valor de la hora");
+            VH = int.Parse(Console.ReadLine());
+            SS = CH * VH;
+            Console.WriteLine($"el sueldo semanal es {SS}");
+
+
+
+        }
+
+    }
+
+    }
